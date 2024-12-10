fe9cbb0dc044b4b3b9ed35a893ecd905b766156f
Justin L <justinian.the.iii@gmail.com>
2024-12-10 15:24:43 -0800

    Switch to @require

--- CMD_LVL/CMD_LVL.user.js
+++ CMD_LVL/CMD_LVL.user.js
@@ -6,12 +6,5 @@
 // @author       Bobtron
 // @match        https://elgea.illyriad.co.uk/*
 // @grant        none
-// @downloadURL  https://raw.githubusercontent.com/Bobtron/IllyriadUserscripts/refs/heads/master/CMD_LVL/CMD_LVL.user.js
-// @updateURL    https://raw.githubusercontent.com/Bobtron/IllyriadUserscripts/refs/heads/master/CMD_LVL/CMD_LVL.user.js
+// @require      https://raw.githubusercontent.com/Bobtron/IllyriadUserscripts/refs/heads/master/CMD_LVL/CMD_LVL.js
 // ==/UserScript==
-
-var js = `https://raw.githubusercontent.com/Bobtron/IllyriadUserscripts/refs/heads/master/CMD_LVL/CMD_LVL.js`
-var script = document.createElement('script');
-script.setAttribute('src',js);
-script.setAttribute('type','text/javascript');
-document.getElementsByTagName('head')[0].appendChild(script);
